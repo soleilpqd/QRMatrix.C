f7b131e8cb800ed8596444e5cc0e70e1e50e5d28
soleilpqd <soleilpqd@gmail.com>
2023-12-06 05:18:58 +0700

    Fix Masking

--- QRMatrix/qrmatrixboard.c
+++ QRMatrix/qrmatrixboard.c
@@ -308,51 +308,34 @@ void QrmBoard_placeData(
     bool isCompleted = false;
 
     while (column >= 0 && !isCompleted) {
+        int startValue = 0;
+        int endValue = board.dimension - 1;
+        int step = 1;
         if (isUpward) {
-            for (int row = board.dimension - 1; row >= 0 && !isCompleted; row -= 1) {
-                if (buffer[row][column] == CellNeutral) {
-                    QrmBoard_fillDataBit(
-                        board, data, errorCorrection, phase,
-                        &byteIndex, &bitIndex, row, column
+            startValue = board.dimension - 1;
+            endValue = 0;
+            step = -1;
+        }
+        for (int row = startValue; (isUpward ? row >= endValue : row <= endValue) && !isCompleted; row += step) {
+            if (buffer[row][column] == CellNeutral) {
+                QrmBoard_fillDataBit(
+                    board, data, errorCorrection, phase,
+                    &byteIndex, &bitIndex, row, column
                     );
-                    isCompleted = QrmBoard_checkFilledBit(
-                        &bitCount, dataBitTotal, ecBitTotal, &byteIndex,
-                        &bitIndex, &phase, remainderCount
+                isCompleted = QrmBoard_checkFilledBit(
+                    &bitCount, dataBitTotal, ecBitTotal, &byteIndex,
+                    &bitIndex, &phase, remainderCount
                     );
-                }
-                if (column > 0 && buffer[row][column - 1] == CellNeutral) {
-                    QrmBoard_fillDataBit(
-                        board, data, errorCorrection, phase,
-                        &byteIndex, &bitIndex, row, column - 1
-                    );
-                    isCompleted = QrmBoard_checkFilledBit(
-                        &bitCount, dataBitTotal, ecBitTotal, &byteIndex,
-                        &bitIndex, &phase, remainderCount
-                    );
-                }
             }
-        } else {
-            for (int row = 0; row < board.dimension && !isCompleted; row += 1) {
-                if (buffer[row][column] == CellNeutral) {
-                    QrmBoard_fillDataBit(
-                        board, data, errorCorrection, phase,
-                        &byteIndex, &bitIndex, row, column
+            if (column > 0 && buffer[row][column - 1] == CellNeutral) {
+                QrmBoard_fillDataBit(
+                    board, data, errorCorrection, phase,
+                    &byteIndex, &bitIndex, row, column - 1
                     );
-                    isCompleted = QrmBoard_checkFilledBit(
-                        &bitCount, dataBitTotal, ecBitTotal, &byteIndex,
-                        &bitIndex, &phase, remainderCount
+                isCompleted = QrmBoard_checkFilledBit(
+                    &bitCount, dataBitTotal, ecBitTotal, &byteIndex,
+                    &bitIndex, &phase, remainderCount
                     );
-                }
-                if (column > 0 && buffer[row][column - 1] == CellNeutral) {
-                    QrmBoard_fillDataBit(
-                        board, data, errorCorrection, phase,
-                        &byteIndex, &bitIndex, row, column - 1
-                    );
-                    isCompleted = QrmBoard_checkFilledBit(
-                        &bitCount, dataBitTotal, ecBitTotal, &byteIndex,
-                        &bitIndex, &phase, remainderCount
-                    );
-                }
             }
         }
         column -= 2;
@@ -373,6 +356,7 @@ UnsignedByte** QrmBoard_mask(QrmBoard board, UnsignedByte maskNum) {
         for (UnsignedByte column = 0; column < board.dimension; column += 1) {
             UnsignedByte byte = buffer[row][column];
             UnsignedByte low = byte & CellLowMask;
+            UnsignedByte high = byte & CellHighMask;
             bool isFunc = (byte & CellFuncMask) > 0;
             if (isFunc) {
                 result[row][column] = byte;
@@ -383,7 +367,7 @@ UnsignedByte** QrmBoard_mask(QrmBoard board, UnsignedByte maskNum) {
             } else if (low == CellUnset) {
                 low = CellSet;
             }
-            UnsignedByte maskedByte = low;
+            UnsignedByte maskedByte = low | high;
             switch (maskNum) {
             case 0:
                 if (((row + column) % 2) == 0) {
